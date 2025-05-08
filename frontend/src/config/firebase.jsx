6612d80b6baf4ddb9b888ae0d0329de0e957a135
Viviana Hoyos <vhoyoss@eafit.edu.co>
2025-05-08 10:24:34 -0500

    firebase as json

--- frontend/src/config/firebase.jsx
+++ frontend/src/config/firebase.jsx
@@ -1,14 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBsYG-Kf29UwuLOekxHr9BGISwoNPfnv7I",
-    authDomain: "proyecto-pvc.firebaseapp.com",
-    projectId: "proyecto-pvc",
-    storageBucket: "proyecto-pvc.firebasestorage.app",
-    messagingSenderId: "242133432177",
-    appId: "1:242133432177:web:d9800add14a832792628d0"
-};
+import firebaseConfig from './firebaseConfig.json';
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
